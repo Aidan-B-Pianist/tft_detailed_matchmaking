09e43128bab88c854cebd8d122f217506eb9fa6f
Aidan Burchett <aidan@burchett.me>
2026-03-03 11:19:13 -0600

    test commit

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
 use std::io::{self, Write};
 use anyhow;
+use serde;
+
+struct SummonerResponse {
+    puuid : String,
+    game_name : String,
+    tag_line1 : String
+}
+
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -20,7 +28,9 @@ async fn main() -> anyhow::Result<()> {
 
 async fn tft_api_request(summoner: &str) -> anyhow::Result<()> {
     // Placeholder: this is where your pipeline goes:
+    
     // 1) riot-id -> account endpoint (puuid)
+    ///riot/account/v1/accounts/by-riot-id/{gameName}/{tagLine} get the api request format
     // 2) puuid -> matchlist endpoint (match ids)
     // 3) match ids -> match details (concurrently)
     // 4) aggregate “top comps”, print report
@@ -28,3 +38,7 @@ async fn tft_api_request(summoner: &str) -> anyhow::Result<()> {
     println!("(todo) Would query Riot API for: {}", summoner);
     Ok(())
 }
+
+//Only 1 API request for the last game's comp
+//8 * 10 = 80
+//88 requests per run
